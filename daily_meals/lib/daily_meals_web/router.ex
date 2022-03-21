a5b0d5b9a7d3b355924e97bc1c936f2ee312c675
joabehenrique <henriquecidoz@hotmail.com>
2022-03-21 16:24:18 -0300

    feat: added route of create meals

--- daily_meals/lib/daily_meals_web/router.ex
+++ daily_meals/lib/daily_meals_web/router.ex
@@ -2,26 +2,21 @@ defmodule DailyMealsWeb.Router do
   use DailyMealsWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
-  scope "/api", DailyMealsWeb do
-    pipe_through :api
+  scope "/api/v1", DailyMealsWeb do
+    pipe_through(:api)
+
+    post("/meals", MealsController, :create)
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: DailyMealsWeb.Telemetry
+      pipe_through([:fetch_session, :protect_from_forgery])
+      live_dashboard("/dashboard", metrics: DailyMealsWeb.Telemetry)
     end
   end
 end

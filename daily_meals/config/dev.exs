eb120616e186bedfcd4a68db67312df54f83c096
joabehenrique <henriquecidoz@hotmail.com>
2022-03-21 16:20:16 -0300

    chore: set up database communication

--- daily_meals/config/dev.exs
+++ daily_meals/config/dev.exs
@@ -2,10 +2,11 @@ use Mix.Config
 
 # Configure your database
 config :daily_meals, DailyMeals.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "admin",
+  password: "admin",
   database: "daily_meals_dev",
   hostname: "localhost",
+  port: "5433",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 

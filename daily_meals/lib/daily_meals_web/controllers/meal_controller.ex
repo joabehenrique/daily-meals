8707a47a698b91d2d1af679a2a276c6d21710549
joabehenrique <henriquecidoz@hotmail.com>
2022-03-22 13:35:55 -0300

    feat: added controllers of update, delete, find..

--- daily_meals/lib/daily_meals_web/controllers/meal_controller.ex
+++ daily_meals/lib/daily_meals_web/controllers/meal_controller.ex
@@ -8,11 +8,41 @@ defmodule DailyMealsWeb.MealsController do
 
   def create(conn, params) do
     with {:ok, %Meals{} = meal} <- DailyMeals.create_meal(params) do
-      IO.inspect(meal, label: "::::::::::AQUI:::::::")
-
       conn
       |> put_status(:created)
       |> render("create.json", meal: meal)
+    end
+  end
+
+  def update(conn, params) do
+    with {:ok, %Meals{} = meal} <- DailyMeals.update_meal(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", meal: meal)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    with {:ok, %Meals{} = meal} <- DailyMeals.delete_meal(id) do
+      conn
+      |> put_status(:no_content)
+      |> text("")
+    end
+  end
+
+  def find_all(conn, _params) do
+    with {:ok, meal} <- DailyMeals.find_all_meal() do
+      conn
+      |> put_status(:ok)
+      |> render("find_all.json", meal: meal)
+    end
+  end
+
+  def find_by_id(conn, %{"id" => id}) do
+    with {:ok, %Meals{} = meal} <- DailyMeals.find_by_id_meal(id) do
+      conn
+      |> put_status(:ok)
+      |> render("find_by_id.json", meal: meal)
     end
   end
 end

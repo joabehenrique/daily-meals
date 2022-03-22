ccda9cc9db1e0f97874ca4a68ba74ca44ea95226
joabehenrique <henriquecidoz@hotmail.com>
2022-03-22 13:33:20 -0300

    feat: added delegate of update, delete..

--- daily_meals/lib/daily_meals.ex
+++ daily_meals/lib/daily_meals.ex
@@ -1,5 +1,13 @@
 defmodule DailyMeals do
   alias DailyMeals.Meals.Create, as: MealCreate
+  alias DailyMeals.Meals.Update, as: MealUpdate
+  alias DailyMeals.Meals.Delete, as: MealDelete
+  alias DailyMeals.Meals.FindAll, as: MealFindAll
+  alias DailyMeals.Meals.FindById, as: MealFindById
 
   defdelegate create_meal(params), to: MealCreate, as: :call
+  defdelegate update_meal(params), to: MealUpdate, as: :call
+  defdelegate delete_meal(params), to: MealDelete, as: :call
+  defdelegate find_all_meal(), to: MealFindAll, as: :call
+  defdelegate find_by_id_meal(params), to: MealFindById, as: :call
 end

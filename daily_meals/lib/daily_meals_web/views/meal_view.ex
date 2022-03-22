02f70a3362e5b98cc899cc7242b28a02ff8a395c
joabehenrique <henriquecidoz@hotmail.com>
2022-03-22 13:36:34 -0300

    feat: added render of update, delete, find..

--- daily_meals/lib/daily_meals_web/views/meal_view.ex
+++ daily_meals/lib/daily_meals_web/views/meal_view.ex
@@ -8,4 +8,25 @@ defmodule DailyMealsWeb.MealsView do
       message: "Meal created",
       meal: meal
     }
+
+  def render("find_all.json", %{meal: meal}) do
+    %{
+      message: "Meal found",
+      meal: meal
+    }
+  end
+
+  def render("find_by_id.json", %{meal: %Meals{} = meal}) do
+    %{
+      message: "Meal found",
+      meal: meal
+    }
+  end
+
+  def render("update.json", %{meal: %Meals{} = meal}) do
+    %{
+      message: "Meal updated",
+      meal: meal
+    }
+  end
 end

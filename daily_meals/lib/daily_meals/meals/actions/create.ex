865015ad2bb328c107bd1793925a91308d56b87c
joabehenrique <henriquecidoz@hotmail.com>
2022-03-24 22:29:53 -0300

    feat: added validation if user exists

--- daily_meals/lib/daily_meals/meals/actions/create.ex
+++ daily_meals/lib/daily_meals/meals/actions/create.ex
@@ -1,8 +1,16 @@
 defmodule DailyMeals.Meals.Create do
   alias DailyMeals.{Error, Repo}
   alias DailyMeals.Meals
+  alias DailyMeals.User
 
-  def call(params) do
+  def call(%{"user_id" => id} = params) do
+    case Repo.get(User, id) do
+      nil -> {:error, Error.build_user_not_found_error()}
+      _user -> create(params)
+    end
+  end
+
+  defp create(params) do
     params
     |> Meals.changeset()
     |> Repo.insert()
